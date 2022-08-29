1288631bae1fcba2ab49bd39ab21e4cee5a0b086
rupeshshinde22 <rupesh.shinde@springernature.com>
2022-08-29 14:35:42 +0530

    scala assignment started

---
+++ src/main/scala/helloWorld.scala
@@ -0,0 +1,20 @@
+object helloWorld extends App
+{
+//  println("Hello world!");
+//  for (i<-0 to 10)
+//    println(i)
+//
+//    println("Welcome to the Scala worksheet")
+
+
+  println("3".toInt)
+//  println("foo".toInt)
+  println("abcdef".take(3))
+
+
+//
+//    if(20 > 10) println("left") else println("right")
+//    println("The ultimate answer is " + 42)
+
+
+}
---
+++ src/main/scala/try1.scala
@@ -0,0 +1,7 @@
+class try1 {
+val result=for (i<-0 to 10)
+  println(i)
+}
+object try2 extends App {
+
+}
