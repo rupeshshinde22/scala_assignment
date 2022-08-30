7fd62e346e712ca20d0d27384d8a0912f51c8f15
rupeshshinde22 <rupesh.shinde@springernature.com>
2022-08-30 13:54:03 +0530

    Define an object called calc with a method square that accepts a Double as an argument and... you guessed it... squares its input. Add a method called cube that cubes its input calling square as part of its result calcula􏰀on.

--- src/main/scala/EXPRESSIONS_TYPES_VALUES/Cat.scala
+++ src/main/scala/EXPRESSIONS_TYPES_VALUES/Cat.scala
@@ -1,5 +1,18 @@
 package EXPRESSIONS_TYPES_VALUES
 
-class Cat {
+object Oswald{
+  val colour=println("Black")
+  val food=println("Milk")
+}
+object Henderson{
+  val colour=println("Ginger")
+  val food=println("Chips")
+}
+object Quentin  {
+  val colour=println("Tabby and white")
+  val food=println("Curry")
+}
 
+object Catdetails extends App {
+  Oswald.colour
 }
---
+++ src/main/scala/EXPRESSIONS_TYPES_VALUES/Cube_Square.scala
@@ -0,0 +1,5 @@
+package EXPRESSIONS_TYPES_VALUES
+
+class Cube_Square {
+
+}
