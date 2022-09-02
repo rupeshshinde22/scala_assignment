be0f623f5efe2bacbf9de31c0211b9ab330ceb4d
rupeshshinde22 <rupesh.shinde@springernature.com>
2022-09-02 18:13:22 +0530

    precise square question completed as we have created 2 method with the same name but different argumensts as it can take Int and Double

--- src/main/scala/EXPRESSIONS_TYPES_VALUES/Precise_Square.scala
+++ src/main/scala/EXPRESSIONS_TYPES_VALUES/Precise_Square.scala
@@ -3,3 +3,22 @@ package EXPRESSIONS_TYPES_VALUES
 class Precise_Square {
 
 }
+object calc2 {
+  def square(num:Double) = {
+    val result = num * num
+    println(result)
+  }
+  def square(num:Int) = {
+    val result = num * num
+    println(result)
+  }
+//  def cube(num:Double): Unit = {
+//    val result= num* num * num
+//    println(result)
+//  }
+}
+object main1 extends App {
+  calc2.square(9.2)
+//  calc2.cube(8)
+
+}
